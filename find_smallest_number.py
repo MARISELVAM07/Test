4884262b1b611d27334fb26a95c64de5922ac690
mariselvam07 <rlmselvam7@gmail.com>
2022-09-12 08:53:43 -0700

    new file added

---
+++ find_smallest_number.py
@@ -0,0 +1,12 @@
+number1 = int(input('Enter First number : '))
+number2 = int(input('Enter Second number : '))
+number3 = int(input('Enter Third number : '))
+def smallest(num1, num2, num3):
+    if (num1 < num2) and (num1 < num3):
+        smallest_num = num1
+    elif (num2 < num1) and (num2 < num3):
+        smallest_num = num2
+    else:
+        smallest_num = num3
+    print("The smallest of the 3 numbers is : ", smallest_num)
+smallest(number1, number2, number3)
