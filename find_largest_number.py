e3ab0bad8a95e6e989629bd2a5cd4dd4bce7f247
mariselvam07 <rlmselvam7@gmail.com>
2022-09-13 07:08:18 -0700

    added 4th number

--- find_largest_number.py
+++ find_largest_number.py
@@ -1,12 +1,15 @@
 number1 = int(input('Enter First Number : '))
 number2 = int(input('Enter Second Number : '))
 number3 = int(input('Enter Third Number : '))
-def largest(num1, num2, num3):
-    if (num1 > num2) and (num1 > num3):
+number4 = int(input('Enter Fourth Number : '))
+def largest(num1, num2, num3, num4):
+    if (num1 > num2) and (num1 > num3) and (num1 > num4):
         largest_num = num1
-    elif (num2 > num1) and (num2 > num3):
+    elif (num2 > num1) and (num2 > num3) and (num2 > num4):
         largest_num = num2
-    else:
+    elif (num3 > num1) and (num3 > num2) and (num3 > num4):
         largest_num = num3
-    print("The largest of the 3 numbers is : ", largest_num)
-largest(number1, number2, number3)
+    else:
+        largest_num = num4
+    print("The largest of the 4 numbers is : ", largest_num)
+largest(number1, number2, number3, number4)
