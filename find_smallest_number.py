a4c10d27793f0355bc571a452b4e123a4ee43d1b
MARISELVAM07 <112251969+MARISELVAM07@users.noreply.github.com>
2022-09-12 18:51:32 -0700

    added number4

--- find_smallest_number.py
+++ find_smallest_number.py
@@ -1,12 +1,15 @@
 number1 = int(input('Enter First number : '))
 number2 = int(input('Enter Second number : '))
 number3 = int(input('Enter Third number : '))
-def smallest(num1, num2, num3):
-    if (num1 < num2) and (num1 < num3):
+number4 = int(input('Enter Fourth number : '))
+def smallest(num1, num2, num3, num4):
+    if (num1 < num2) and (num1 < num3) and (num1 < num4):
         smallest_num = num1
-    elif (num2 < num1) and (num2 < num3):
+    elif (num2 < num1) and (num2 < num3) and (num2 < num4):
         smallest_num = num2
-    else:
+    elif (num3 < num1) and (num3 < num2) and (num3 < num4):
         smallest_num = num3
+    else:
+        smallest_num = num4
     print("The smallest of the 3 numbers is : ", smallest_num)
-smallest(number1, number2, number3)
+smallest(number1, number2, number3, number4)
